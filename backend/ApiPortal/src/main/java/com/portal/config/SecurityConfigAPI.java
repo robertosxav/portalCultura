e150937c2769b07247b31d1a7a7d13118368fa3e
robertosxav <roberto.sxav@gmail.com>
2023-08-14 21:45:31 -0400

    correções gerais

--- backend/ApiPortal/src/main/java/com/portal/config/SecurityConfigAPI.java
+++ backend/ApiPortal/src/main/java/com/portal/config/SecurityConfigAPI.java
@@ -3,6 +3,7 @@ package com.portal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,6 +19,13 @@ public class SecurityConfigAPI {
 							try {
 								authorizeRequests
 								.requestMatchers("/publicos/**").permitAll()
+								.requestMatchers("/swagger-ui.html").permitAll()
+								.requestMatchers("/swagger-ui/**").permitAll()
+				                .requestMatchers("/webjars/**").permitAll()
+				                .requestMatchers("/content/**").permitAll()
+				                .requestMatchers("/swagger-resources/**").permitAll()
+				                .requestMatchers("/api-docs/**").permitAll()
+				                .requestMatchers(HttpMethod.GET, "/v3/**").permitAll()
 								 .anyRequest().authenticated()
 							 	 .and()
 						         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
